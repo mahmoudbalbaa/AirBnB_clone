b491b8c808c91f6056173fa1f26e0ae4af3dc902
gamal sob <133385330+gamalsobih@users.noreply.github.com>
2024-01-12 04:14:36 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -7,9 +7,8 @@ serve as the base of our model.
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
-
-
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
