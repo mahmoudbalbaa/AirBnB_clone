c638b04de726782005d14231deee1c71656cd662
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2024-01-14 15:51:54 -0800

    test place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,154 +1,80 @@
-#!/usr/bin/env python3
-"""Test model for Place class"""
-
+#!/usr/bin/python3
+"""
+Test model for Place class
+"""
 import unittest
-import os
 from models.place import Place
-from datetime import datetime
-from time import sleep
-from models.base_model import BaseModel
-import uuid
 
 
 class TestPlace(unittest.TestCase):
-    """Place model class test case"""
-
-    @classmethod
-    def setUpClass(cls):
+    """
+    Place model class test case
+    """
+    def setUp(self):
         """Setup the unittest"""
-        cls.place = Place()
-        cls.place.city_id = str(uuid.uuid4())
-        cls.place.user_id = str(uuid.uuid4())
-        cls.place.name = "Any place in the world"
-        cls.place.description = "Suny Beatch"
-        cls.place.number_rooms = 0
-        cls.place.number_bathrooms = 0
-        cls.place.max_guest = 0
-        cls.place.price_by_night = 0
-        cls.place.latitude = 0.0
-        cls.place.longitude = 0.0
-        cls.place.amenity_ids = []
 
-    @classmethod
-    def tearDownClass(cls):
-        """Clean up the dirt"""
-        del cls.place
-        try:
-            os.remove("file.json")
-        except FileNotFoundError:
-            pass
+        self.place = Place()
 
-    def test_is_subclass(self):
-        self.assertTrue(issubclass(self.place.__class__, BaseModel))
+    def test_attributes(self):
+        """
+        test place attributes
+        """
 
-    def checking_for_doc(self):
-        self.assertIsNotNone(Place.__doc__)
+        self.assertTrue(hasattr(self.place, 'id'))
+        self.assertTrue(hasattr(self.place, 'created_at'))
+        self.assertTrue(hasattr(self.place, 'updated_at'))
+        self.assertTrue(hasattr(self.place, 'city_id'))
+        self.assertTrue(hasattr(self.place, 'user_id'))
+        self.assertTrue(hasattr(self.place, 'name'))
+        self.assertTrue(hasattr(self.place, 'description'))
+        self.assertTrue(hasattr(self.place, 'number_rooms'))
+        self.assertTrue(hasattr(self.place, 'number_bathrooms'))
+        self.assertTrue(hasattr(self.place, 'max_guest'))
+        self.assertTrue(hasattr(self.place, 'price_by_night'))
+        self.assertTrue(hasattr(self.place, 'latitude'))
+        self.assertTrue(hasattr(self.place, 'longitude'))
+        self.assertTrue(hasattr(self.place, 'amenity_ids'))
 
-    def test_has_attributes(self):
-        self.assertTrue('id' in self.place.__dict__)
-        self.assertTrue('created_at' in self.place.__dict__)
-        self.assertTrue('updated_at' in self.place.__dict__)
-        self.assertTrue('city_id' in self.place.__dict__)
-        self.assertTrue('user_id' in self.place.__dict__)
-        self.assertTrue('name' in self.place.__dict__)
-        self.assertTrue('max_guest' in self.place.__dict__)
-        self.assertTrue('price_by_night' in self.place.__dict__)
-        self.assertTrue('latitude' in self.place.__dict__)
-        self.assertTrue('longitude' in self.place.__dict__)
-        self.assertTrue('amenity_ids' in self.place.__dict__)
-        self.assertTrue('description' in self.place.__dict__)
-        self.assertTrue('number_rooms' in self.place.__dict__)
-        self.assertTrue('number_bathrooms' in self.place.__dict__)
+    def test_save_method(self):
+        """test saving method"""
 
-    def test_attributes_are_string(self):
-        self.assertIs(type(self.place.city_id), str)
-        self.assertIs(type(self.place.user_id), str)
-        self.assertIs(type(self.place.name), str)
-        self.assertIs(type(self.place.description), str)
-        self.assertIs(type(self.place.number_rooms), int)
-        self.assertIs(type(self.place.max_guest), int)
-        self.assertIs(type(self.place.price_by_night), int)
-        self.assertIs(type(self.place.latitude), float)
-        self.assertIs(type(self.place.longitude), float)
-        self.assertIs(type(self.place.amenity_ids), list)
-
-    def test_save(self):
+        before_save = self.place.updated_at
         self.place.save()
-        self.assertNotEqual(self.place.created_at, self.place.updated_at)
+        after_save = self.place.updated_at
+        self.assertNotEqual(before_save, after_save)
 
-    def test_one_save(self):
-        pl = Place()
-        sleep(0.05)
-        first_updated_at = pl.updated_at
-        pl.save()
-        self.assertLess(first_updated_at, pl.updated_at)
+    def test_to_dict_method(self):
+        """test dict methods"""
 
-    def test_two_saves(self):
-        pl = Place()
-        sleep(0.05)
-        first_updated_at = pl.updated_at
-        pl.save()
-        second_updated_at = pl.updated_at
-        self.assertLess(first_updated_at, second_updated_at)
-        sleep(0.05)
-        pl.save()
-        self.assertLess(second_updated_at, pl.updated_at)
+        place_dict = self.place.to_dict()
+        self.assertTrue(isinstance(place_dict, dict))
+        self.assertIn('__class__', place_dict)
+        self.assertIn('id', place_dict)
+        self.assertIn('created_at', place_dict)
+        self.assertIn('updated_at', place_dict)
+        self.assertIn('city_id', place_dict)
+        self.assertIn('user_id', place_dict)
+        self.assertIn('name', place_dict)
+        self.assertIn('description', place_dict)
+        self.assertIn('number_rooms', place_dict)
+        self.assertIn('number_bathrooms', place_dict)
+        self.assertIn('max_guest', place_dict)
+        self.assertIn('price_by_night', place_dict)
+        self.assertIn('latitude', place_dict)
+        self.assertIn('longitude', place_dict)
+        self.assertIn('amenity_ids', place_dict)
 
-    def test_save_with_arg(self):
-        pl = Place()
-        with self.assertRaises(TypeError):
-            pl.save(None)
+    def test_str_method(self):
+        """test str representation"""
 
-
-class TestPlace_to_dict(unittest.TestCase):
-    """Unittests for testing to_dict method of the Place class."""
-
-    def test_to_dict_type(self):
-        self.assertTrue(dict, type(Place().to_dict()))
-
-    def test_to_dict_contains_correct_keys(self):
-        pl = Place()
-        self.assertIn("id", pl.to_dict())
-        self.assertIn("created_at", pl.to_dict())
-        self.assertIn("updated_at", pl.to_dict())
-        self.assertIn("__class__", pl.to_dict())
-
-    def test_to_dict_contains_added_attributes(self):
-        pl = Place()
-        pl.middle_name = "Holberton"
-        pl.my_number = 98
-        self.assertEqual("Holberton", pl.middle_name)
-        self.assertIn("my_number", pl.to_dict())
-
-    def test_to_dict_datetime_attributes_are_strs(self):
-        pl = Place()
-        pl_dict = pl.to_dict()
-        self.assertEqual(str, type(pl_dict["id"]))
-        self.assertEqual(str, type(pl_dict["created_at"]))
-        self.assertEqual(str, type(pl_dict["updated_at"]))
-
-    def test_to_dict_output(self):
-        dt = datetime.today()
-        pl = Place()
-        pl.id = "123456"
-        pl.created_at = pl.updated_at = dt
-        tdict = {
-            'id': '123456',
-            '__class__': 'Place',
-            'created_at': dt.isoformat(),
-            'updated_at': dt.isoformat(),
-        }
-        self.assertDictEqual(pl.to_dict(), tdict)
-
-    def test_contrast_to_dict_dunder_dict(self):
-        pl = Place()
-        self.assertNotEqual(pl.to_dict(), pl.__dict__)
-
-    def test_to_dict_with_arg(self):
-        pl = Place()
-        with self.assertRaises(TypeError):
-            pl.to_dict(None)
+        str_representation = str(self.place)
+        self.assertTrue(isinstance(str_representation, str))
+        self.assertIn(self.place.__class__.__name__, str_representation)
+        self.assertIn(self.place.id, str_representation)
+        self.assertIn('id', str_representation)
+        self.assertIn('created_at', str_representation)
+        self.assertIn('updated_at', str_representation)
 
 
-if __name__ == "__main__":
-    unittest.main()
+if __name__ == '__main__':
+    unitt
