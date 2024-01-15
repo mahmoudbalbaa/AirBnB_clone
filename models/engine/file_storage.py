7cf5415f43946a587635f079c727a32e3024da66
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2024-01-14 19:12:38 -0800

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-
+FileStorage module for storing and retrieving instances
 """
 
 import json
@@ -16,59 +16,51 @@ from models.review import Review
 
 class FileStorage:
     """
-
+    FileStorage class for storing and retrieving instances
     """
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """
-
+        Return the dictionary __objects
         """
         return FileStorage.__objects
 
     def new(self, obj):
         """
-
+        Set in __objects the obj with key <obj class name>.id
         """
-        obj_cls_name = obj.__class__.__name__
-        key = "{}.{}".format(obj_cls_name, obj.id)
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
         """
-
+        Serialize __objects to the JSON file (path: __file_path)
         """
-        all_objs = FileStorage.__objects
-        obj_dict = {}
-
-        for obj in all_objs.keys():
-            obj_dict[obj] = all_objs[obj].to_dict()
-
+        obj_dict = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             json.dump(obj_dict, f)
 
     def reload(self):
         """
-
+        Deserialize the JSON file to __objects, if the file exists
         """
         if os.path.isfile(FileStorage.__file_path):
             with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
                 try:
                     obj_dict = json.load(f)
 
-                    for key, value in obj_dict.items():
+                    for key, values in obj_dict.items():
                         class_name, obj_id = key.split('.')
+                        cls = globals().get(class_name)
 
-                        if class_name in globals():
-                            cls = globals()[class_name]
+                        if cls:
+                            instance = cls(**values)
+                            FileStorage.__objects[key] = instance
                         else:
-                            print(f"Warning: Unknown class name "
-                                  f"'{class_name}' in file.")
-                            continue
+                            print(f"Warning: Unknown class name '{class_name}' in file.")
+                except Exception as e:
+                    print(f"Error reloading: {e}")
 
-                        instance = cls(**values)
-
-                        FileStorage.__objects[key] = instance
-                except Exception:
-                    pass
