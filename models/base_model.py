55a916dbca4d7ff15fb95c219c873594b9737f8d
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2024-01-16 08:47:42 -0800

    update basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -36,7 +36,6 @@ class BaseModel:
 
         self.updated_at = datetime.utcnow()
         models.storage.save()
-        pass
 
     def to_dict(self):
         """Generate a new dict with an extra field __class__"""
